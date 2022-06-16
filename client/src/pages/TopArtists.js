f8ec93f88ab97c20f9ec3bb344b261f552e88fd2
Josue Belacy <jbelacy@gmail.com>
2022-06-15 21:07:16 -0400

    Top Artists button info added

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -18,6 +18,12 @@ const TopArtists = () => {
 
   return (
     <main>
+      <ul>
+        <li><button className={activeRange === 'short' ? 'active': ''} onClick={() => setActiveRange('short')}>This Month</button></li>
+        <li><button className={activeRange === 'medium' ? 'active': ''} onClick={() => setActiveRange('medium')}>Last 6 Months</button></li>
+        <li><button className={activeRange === 'long' ? 'active': ''} onClick={() => setActiveRange('long')}>All Time</button></li>
+      </ul>
+
       <SectionWrapper title="Top Artists" breadcrumb={true}>
         <TimeRangeButtons
           activeRange={activeRange}
